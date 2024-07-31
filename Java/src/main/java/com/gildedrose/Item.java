4fd82ebbad8ed07574feee401d0bef4f07e73a5c
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-07-31 15:06:25 +0530

    Refactor: Add switch case

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 public class Item {
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     public String name;
 
     public int sellIn;
@@ -15,70 +18,69 @@ public class Item {
     }
 
     void updateInventory() {
-        boolean isAgedBrie = name.equals("Aged Brie");
-        if (isAgedBrie) {
-            if (quality < 50) {
-                quality = quality + 1;
+        switch(name) {
+            case AGED_BRIE:
+                updateAgedBrie();
+                break;
+            case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
+                updateBackstagePasses();
+                break;
+            case SULFURAS_HAND_OF_RAGNAROS:
+                break;
+            default:
+                updateRegularItems();
+                break;
+        }
+    }
 
-            }
-
+    private void updateRegularItems() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
             sellIn = sellIn - 1;
 
-            if (sellIn < 0) {
+        if (sellIn < 0) {
+            if (quality > 0) {
+                    quality = quality - 1;
+            }
+        }
+    }
+
+    private void updateBackstagePasses() {
+        if (quality < 50) {
+            quality = quality + 1;
+
+            if (sellIn < 11) {
                 if (quality < 50) {
                     quality = quality + 1;
                 }
             }
-        } else {
-            boolean isBackstagePasses = name.equals("Backstage passes to a TAFKAL80ETC concert");
-            if (isBackstagePasses) {
-                if (!isBackstagePasses) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    if (quality < 50) {
-                        quality = quality + 1;
 
-                        if (sellIn < 11) {
-                            if (quality < 50) {
-                                quality = quality + 1;
-                            }
-                        }
-
-                        if (sellIn < 6) {
-                            if (quality < 50) {
-                                quality = quality + 1;
-                            }
-                        }
-                    }
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
                 }
+            }
+        }
 
-                sellIn = sellIn - 1;
+        sellIn = sellIn - 1;
 
-                if (sellIn < 0) {
-                    quality = 0;
-                }
-            } else {
-                if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
-                    }
-                }
+        if (sellIn < 0) {
+            quality = 0;
+        }
+    }
 
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    sellIn = sellIn - 1;
-                }
+    private void updateAgedBrie() {
+        if (quality < 50) {
+            quality = quality + 1;
 
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
-                    }
-                }
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1;
             }
         }
     }
