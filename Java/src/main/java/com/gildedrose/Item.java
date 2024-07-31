45625cf7097eefd78e6f5b574d387dda4b3a8526
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-07-31 14:52:24 +0530

    Refactor: Lift conditional isBackstagePasses to remove duplicates.

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -30,17 +30,18 @@ public class Item {
                 }
             }
         } else {
-            if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
+            boolean isBackstagePasses = name.equals("Backstage passes to a TAFKAL80ETC concert");
+            if (isBackstagePasses) {
+                if (!isBackstagePasses) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            quality = quality - 1;
+                        }
                     }
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
+                } else {
+                    if (quality < 50) {
+                        quality = quality + 1;
 
-                    if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (sellIn < 11) {
                             if (quality < 50) {
                                 quality = quality + 1;
@@ -54,21 +55,29 @@ public class Item {
                         }
                     }
                 }
-            }
 
-            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 sellIn = sellIn - 1;
-            }
 
-            if (sellIn < 0) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+            } else {
+                if (quality > 0) {
+                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                        quality = quality - 1;
+                    }
+                }
+
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    sellIn = sellIn - 1;
+                }
+
+                if (sellIn < 0) {
                     if (quality > 0) {
                         if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                             quality = quality - 1;
                         }
                     }
-                } else {
-                    quality = 0;
                 }
             }
         }
