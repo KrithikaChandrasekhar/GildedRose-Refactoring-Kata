2aa6b297c7538cdba010d6e0f623bb04f91b15b8
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-07-31 14:26:34 +0530

    Test to check inventory of Backstage passes

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -24,10 +24,19 @@ class GildedRoseTest {
 
     @Test
     void updateInventoryOfAgedBrie() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        Item[] items = { new Item("Aged Brie", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    void updateInventoryOfBackstagePasses() {
+        Item[] items = { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(14, app.items[0].sellIn);
         assertEquals(21, app.items[0].quality);
     }
 
